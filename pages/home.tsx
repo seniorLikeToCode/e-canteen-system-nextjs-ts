1e133164df98a10b183450a8df50698bee44a3fb
Senior <abhishek234543@gmail.com>
2023-02-13 09:54:49 +0530

    home screen updated

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,3 +1,9 @@
+import PropagateLoader from "react-spinners/PropagateLoader";
+
 export default function Home() {
-    return (<h1>Nude pics</h1>);
+    return (
+        <div className="flex h-screen justify-center items-center">
+            <PropagateLoader color='#FF9501' size='25' />
+        </div>
+    );
 }
