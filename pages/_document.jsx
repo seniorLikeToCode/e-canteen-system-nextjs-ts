5d7934d85a2cb67defd499dd82475a5369b211f4
Senior <abhishek234543@gmail.com>
2023-02-15 00:11:43 +0530

    theme support menuBar added

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -2,6 +2,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 import tailwindConfig from "@/tailwind.config";
 
 export default function Document() {
+
+
   return (
     <Html>
       <Head>
