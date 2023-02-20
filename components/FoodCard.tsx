74c4e381e9c58473cda20580d32f0c1cad95e55a
Senior <abhishek234543@gmail.com>
2023-02-20 22:59:33 +0530

    favourites

--- components/FoodCard.tsx
+++ components/FoodCard.tsx
@@ -12,7 +12,6 @@ export default function FoodCard({ category, products }: { category: string; pro
     //  grid grid-cols-2 grid-flow-row gap-x-4 gap-y-4 mt-4 overflow-y-scroll
 
     const addToCart = async (id: string) => {
-        // console.log(`https://ecs-api-dev.onrender.com/api/v1/cart/add/${id}`);
         const addItem = {
             method: 'get',
             url: `https://ecs-api-dev.onrender.com/api/v1/cart/add/${id}`,
@@ -39,6 +38,20 @@ export default function FoodCard({ category, products }: { category: string; pro
         })
     }
 
+    const addToFav = async (id: string) => {
+        const addItem = {
+            method: 'get',
+            url: `https://ecs-api-dev.onrender.com/api/v1/favourites/toggle/${id}`,
+            headers: {
+                Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2U4ZTU3ZjYxZTJmYjI5YTE3MDU4ZDMiLCJlbWFpbCI6InVzZXJAdGVzdC5jb20iLCJyb2xlIjoidXNlciIsImlhdCI6MTY3NjgwNTYxNSwiZXhwIjoxNjc3NDEwNDE1fQ.TlGx79dbOP1mXd1zWAMHJvyQg-4nq6QStSveADaNM0c',
+            },
+        }
+
+        axios(addItem).then((res: any) => {
+            console.log(res.data);
+        })
+    }
+
 
     return (
         <>
@@ -50,12 +63,12 @@ export default function FoodCard({ category, products }: { category: string; pro
                                 return (
                                     <div key={product._id} className='rounded-lg p-2 shadow-md bg-slate-50'>
                                         <div className='flex justify-between'>
-                                            <HiHeart size={25} className='text-neutral-300' />
+                                            <HiHeart size={25} className='text-neutral-300' onClick={() => addToFav(product._id)} />
                                             <HiPlusCircle size={25} className='text-neutral-300' onClick={() => addToCart(product._id)} />
                                         </div>
                                         <div className='flex flex-col items-center mt-3'>
                                             <div className=' w-32 h-32 rounded-full overflow-hidden bg-slate-200'>
-                                                <Image className='mt-3 scale-150' loader={() => product.imageUrl} unoptimized={true} width={120} height={120} src={product.imageUrl} alt="food items" />
+                                                <Image className='mt-3 scale-150' loader={() => product.imageUrl} src={product.imageUrl} unoptimized={true} width={120} height={120} alt={product.product_name} />
                                             </div>
                                         </div>
 
--- pages/favourites.tsx
+++ pages/favourites.tsx
@@ -1,8 +1,13 @@
-import Footer from "@/components/Footer";
-import FoodCard from "@/components/FoodCard";
-import { HiArrowNarrowLeft } from "react-icons/hi";
+const axios = require('axios');
 
-export default function Favourite() {
+import Footer from "@/components/Footer";
+import type { InferGetStaticPropsType, GetServerSideProps } from 'next'
+import { IProducts, IProductData } from '@/types';
+import Image from 'next/image';
+import { HiHeart, HiPlusCircle } from 'react-icons/hi2'
+
+
+export default function Favourite({ products }: InferGetStaticPropsType<typeof getServerSideProps>) {
     return (
         <div className="">
             {/* header */}
@@ -12,9 +17,59 @@ export default function Favourite() {
             </h2>
             {/* main */}
             <div className="m-4">
-                {/* <FoodCard category={ } products={ } /> */}
+                <div className="scroll">
+                    <div className='grid grid-cols-2 gap-x-4 gap-y-4 mt-4 rm-scrollbar drop-shadow mx-1'>
+                        {
+                            products.map((product: IProductData) => {
+                                return (
+                                    <div key={product._id} className='rounded-lg p-2 shadow-md bg-slate-50'>
+                                        <div className='flex justify-between'>
+                                            <HiHeart size={25} className='text-neutral-300' onClick={() => addToFav(product._id)} />
+                                            <HiPlusCircle size={25} className='text-neutral-300' onClick={() => addToCart(product._id)} />
+                                        </div>
+                                        <div className='flex flex-col items-center mt-3'>
+                                            <div className=' w-32 h-32 rounded-full overflow-hidden bg-slate-200'>
+                                                <Image className='mt-3 scale-150' loader={() => product.imageUrl} src={product.imageUrl} unoptimized={true} width={120} height={120} alt={product.product_name} />
+                                            </div>
+                                        </div>
+
+                                        <div className='flex justify-between mt-3 mx-1'>
+                                            <h3 className='text-black font-medium'>{product.product_name}</h3>
+                                            <h1>{product.rate}</h1>
+                                        </div>
+                                    </div>
+                                );
+                            })
+                        }
+                    </div>
+                    <div className='h-48'></div>
+                </div>
             </div>
             <Footer />
         </div>
     );
+}
+
+export const getServerSideProps: GetServerSideProps<{ products: IProducts }> = async () => {
+    // /api/v1/favourites
+    const addItem = {
+        method: 'get',
+        url: `https://ecs-api-dev.onrender.com/api/v1/favourites`,
+        headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2U4ZTU3ZjYxZTJmYjI5YTE3MDU4ZDMiLCJlbWFpbCI6InVzZXJAdGVzdC5jb20iLCJyb2xlIjoidXNlciIsImlhdCI6MTY3NjgwNTYxNSwiZXhwIjoxNjc3NDEwNDE1fQ.TlGx79dbOP1mXd1zWAMHJvyQg-4nq6QStSveADaNM0c',
+        },
+    }
+
+    let fav: any;
+    await axios(addItem).then((res: any) => {
+        fav = res.data;
+    })
+
+    const products: IProducts = fav.data;
+
+    return {
+        props: {
+            products,
+        }
+    }   
 }
